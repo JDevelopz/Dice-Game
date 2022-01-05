774e79c1f5e5bafc5a2454e5e4f3d4569f29d3ad
Jeffrey Meijer <76928228+JDevelopz@users.noreply.github.com>
2022-01-05 14:52:47 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ button.onclick = function() {
   // if the number of image 2 is greater then image 2 change text and the other way
   if (document.querySelector('.img2').getAttribute('src', number) > document.querySelector('.img1').getAttribute('src', number)) {
     document.querySelector('h1').innerHTML = 'Player 2 wins!'
-  } else {
+  } else if (document.querySelector('.img1').getAttribute('src', number) > document.querySelector('.img2').getAttribute('src', number)){
     document.querySelector('h1').innerHTML = 'Player 1 wins!'
+  } else {
+    document.querySelector('h1').innerHTML = 'Draw!'
   }
 }
