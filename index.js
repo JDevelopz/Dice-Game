6bfb3572fbc0f52857f580262b779fba1a74f8fe
Jeff <76928228+JDevelopz@users.noreply.github.com>
2023-01-26 10:42:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-// Get player names 
+//Change player names
+const player1 = document.getElementById('player1');
+const player2 = document.getElementById('player2');
+// init function that runs when application gets launched.
+function init(){
+  // Get player names 
 let person1 = prompt('Player 1, please enter your name');
 let person2 = prompt('Player 2, please enter your name');
-//Change player names
-const player1 = document.querySelector('#player1');
-const player2 = document.querySelector('#player2');
-
-if(person1){
+  
+if(person1 != null){
   player1.innerHTML = person1
 }
-if(person2){
+if(person2 !== null){
   player2.innerHTML = person2;
 }
+};
+init()
 
 // Makes Javascript Activate on button click
 var button = document.querySelector('button');
