4164a3c93b9f044594ed7f1344086b6947f0d223
Jeff <76928228+JDevelopz@users.noreply.github.com>
2023-01-26 10:50:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Change player names
+//Get placeholders for player names.
 const player1 = document.getElementById('player1');
 const player2 = document.getElementById('player2');
 // init function that runs when application gets launched.
@@ -6,7 +6,7 @@ function init(){
   // Get player names 
 let person1 = prompt('Player 1, please enter your name');
 let person2 = prompt('Player 2, please enter your name');
-  
+  // Change placeholders to user names of choice.
 if(person1 != null){
   player1.innerHTML = person1
 }
@@ -16,24 +16,22 @@ if(person2 !== null){
 };
 init()
 
-// Makes Javascript Activate on button click
-var button = document.querySelector('button');
+// Activate on button click
+const button = document.querySelector('button');
 button.onclick = function() {
   // random number generator for image 1
-  var number = Math.random();
+  let number = Math.random();
   number = number * 6;
   number = Math.floor(number) + 1;
+  //Change number img 1;
   document.querySelector('.img1').setAttribute('src', 'images/dice' + number + '.png');
-  // random number generator for image 2
-  var number = Math.random();
-  number = number * 6;
-  number = Math.floor(number) + 1;
+  // Change number img 2;
   document.querySelector('.img2').setAttribute('src', 'images/dice' + number + '.png');
   // if the number of image 2 is greater then image 2 change text and the other way
-  if (document.querySelector('.img2').getAttribute('src', number) > document.querySelector('.img1').getAttribute('src', number)) {
-    document.querySelector('h1').innerHTML = 'Player 2 wins!'
-  } else if (document.querySelector('.img1').getAttribute('src', number) > document.querySelector('.img2').getAttribute('src', number)){
-    document.querySelector('h1').innerHTML = 'Player 1 wins!'
+  if (document.querySelector('.img1').getAttribute('src', number) > document.querySelector('.img1').getAttribute('src', number)) {
+    document.querySelector('h1').innerHTML = person1 + 'wins!'
+  } else if (document.querySelector('.img2').getAttribute('src', number) > document.querySelector('.img2').getAttribute('src', number)){
+    document.querySelector('h1').innerHTML = person2 +  'wins!'
   } else {
     document.querySelector('h1').innerHTML = 'Draw!'
   }
