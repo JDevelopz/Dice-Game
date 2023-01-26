98e1cefd4a8083e0c0e28b00f901287a8df0a54b
Jeff <76928228+JDevelopz@users.noreply.github.com>
2023-01-26 10:58:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -23,14 +23,18 @@ button.onclick = function() {
   let number = Math.random();
   number = number * 6;
   number = Math.floor(number) + 1;
+  // random number generator for image 2
+  let number2 = Math.random();
+  number2 = number * 6;
+  number2 = Math.floor(number) + 1;
   //Change number img 1;
   document.querySelector('.img1').setAttribute('src', 'images/dice' + number + '.png');
   // Change number img 2;
-  document.querySelector('.img2').setAttribute('src', 'images/dice' + number + '.png');
+  document.querySelector('.img2').setAttribute('src', 'images/dice' + number2 + '.png');
   // if the number of image 2 is greater then image 2 change text and the other way
   if (document.querySelector('.img1').getAttribute('src', number) > document.querySelector('.img1').getAttribute('src', number)) {
     document.querySelector('h1').innerHTML = person1 + 'wins!'
-  } else if (document.querySelector('.img2').getAttribute('src', number) > document.querySelector('.img2').getAttribute('src', number)){
+  } else if (document.querySelector('.img2').getAttribute('src', number) > document.querySelector('.img2').getAttribute('src', number2)){
     document.querySelector('h1').innerHTML = person2 +  'wins!'
   } else {
     document.querySelector('h1').innerHTML = 'Draw!'
